262a5f311b496457f04fe6dc5b5917999db0ab33
Narek <narek@mit.edu>
2016-12-02 23:52:55 -0500

    Started implementing JUnit tests for piece1 and piece2

--- test/abc/sound/SequencePlayerTest.java
+++ test/abc/sound/SequencePlayerTest.java
@@ -2,16 +2,67 @@ package abc.sound;
 
 import static org.junit.Assert.*;
 
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiUnavailableException;
+
 import org.junit.Test;
+/**
+ * Tests piece1.abc and piece2.abc
+ * @author narek
+ * @category no_didit
+ *
+ */
 
 public class SequencePlayerTest {
 
-    // TODO: warmup #2
+    // warmup #2
     
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testPiece1() {
+        try {
+            SequencePlayer player = new SequencePlayer(120, 2);
+
+            player.addNote(new Pitch('C').toMidiNote(), 0, 1);
+            player.addNote(new Pitch('C').toMidiNote(), 1, 1);
+            //player.addNote(new Pitch('C3/4').toMidiNote, 2, 1);
+            //player.addNote(new Pitch('D/4').toMidiNote(), 3, 1);
+            player.addNote(new Pitch('E').toMidiNote(), 2, 1);
+
+            System.out.println(player);
+            System.out.println(player.toString());
+
+            // play!
+            player.play();
+            System.exit(0);
+
+        } catch (MidiUnavailableException mue) {
+            mue.printStackTrace();
+        } catch (InvalidMidiDataException imde) {
+            imde.printStackTrace();
+        }
     }
     
+    @Test
+    public void testPiece2() {
+        try {
+            SequencePlayer player = new SequencePlayer(120, 2);
 
+            player.addNote(new Pitch('C').toMidiNote(), 0, 1);
+            player.addNote(new Pitch('D').toMidiNote(), 1, 1);
+            player.addNote(new Pitch('E').toMidiNote(), 2, 1);
+            player.addNote(new Pitch('F').toMidiNote(), 3, 1);
+
+            System.out.println(player);
+            System.out.println(player.toString());
+
+            // play!
+            player.play();
+            System.exit(0);
+
+        } catch (MidiUnavailableException mue) {
+            mue.printStackTrace();
+        } catch (InvalidMidiDataException imde) {
+            imde.printStackTrace();
+        }
+    }
 }
