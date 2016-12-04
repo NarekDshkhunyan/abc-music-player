a48bb8641d00732ee654813e1ed797c64546cc8c
Mohannad Abunassar <mabunass@mit.edu>
2016-12-04 00:12:38 -0500

    Rest.java done!

--- src/abc/sound/Rest.java
+++ src/abc/sound/Rest.java
@@ -1,17 +1,34 @@
 package abc.sound;
 
 public class Rest implements Music {
+    
+    private final double duration;
+    
+    // Abstraction Function: Represents rest for a certain duration given by duration.
+    //                       A rest is an interval of silence in a piece of music.
+    //                       (source: https://en.wikipedia.org/wiki/Rest_(music))
+    //
+    // Representation Invariant: Duration of rest, duration, must be non-negative.
+    //      
+    // Safety from rep exposure: Duration field is made final and private.
+    
+    private void checkRep(){
+        assert this.duration >= 0;
+    }
+    
+    public Rest(double duration){
+        this.duration = duration;
+        checkRep();    
+    }
 
     @Override
     public double duration() {
-        // TODO Auto-generated method stub
-        return 0;
+        return duration;
     }
 
     @Override
     public Music transpose(int semitonesUp) {
-        // TODO Auto-generated method stub
-        return null;
+        return this;
     }
 
     @Override
