5b20e239bafe6d97e861916b5b527d744a966b9a
Narek <narek@mit.edu>
2016-12-07 20:03:55 -0500

    Added getters, checkRep, and improved equals

--- src/abc/sound/Header.java
+++ src/abc/sound/Header.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import expressivo.Number;
-
 /**
  * Header class represents the header of a piece of music in abc notation
  *
